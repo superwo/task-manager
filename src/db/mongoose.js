d490b20daa4236f5d4bd3ada9fcbf264b7aa5f31
avadarad <avadanii@tiscali.it>
2019-06-12 14:36:45 +0300

    sanitization and validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,45 +1,85 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true
-})
+});
 
-const User = mongoose.model('User', {
+const User = mongoose.model("User", {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot contain "password"');
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    }
   }
-})
+});
 
 // const me = new User({
-//   name: 'Radu',
-//   age: 40
-// })
-// me.save().then(() => {
-//   console.log(me);
-// }).catch(error => {
-//   console.log('Error!', error);
-// })
+//   name: "   Mike  ",
+//   email: "mike@MEAD.IO  ",
+//   password: "khsdjhflasf89"
+// });
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch(error => {
+//     console.log("Error!", error);
+//   });
 
-const Task = mongoose.model('Task', {
+const Task = mongoose.model("Task", {
   description: {
-    type: String
+    type: String,
+    trim: true,
+    required: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   }
 });
 
 const task1 = new Task({
-  description: 'First Task',
-  completed: false
+  description: "Task 2",
+  completed: true
 });
 
-task1.save().then(() => {
-  console.log(task1);
-}).catch(error => {
-  console.log(error);
-})
+task1
+  .save()
+  .then(() => {
+    console.log(task1);
+  })
+  .catch(error => {
+    console.log(error);
+  });
